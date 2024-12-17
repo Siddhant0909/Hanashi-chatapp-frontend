338eff2cfef80d528225563d1f1b5679a683129f
Siddhant Khare <siddhantkhare056@gmail.com>
2024-12-18 02:15:42 +0530

    change theme

--- src/store/useThemeStore.js
+++ src/store/useThemeStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 
 export const useThemeStore = create((set) => ({
-  theme: localStorage.getItem("chat-theme") || "Wireframe",
+  theme: localStorage.getItem("chat-theme") || "wireframe",
   setTheme: (newTheme) => {
     localStorage.setItem("chat-theme", newTheme);
     set({ theme: newTheme });
